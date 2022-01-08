04d380527b2ea548bb17aeebd964dc263adfc8c0
Iris <j58701076@yahoo.com.tw>
2022-01-09 00:26:25 +0800

    第二題算平均薪資
    第二題算平均薪資Python跟JavaScript

---
+++ class02.py
@@ -0,0 +1,25 @@
+def avg(data):
+    total=0
+    # print(total)
+    mycount=data["count"]
+    for n in range(mycount):
+        total+=data["employees"][n]["salary"]
+    print("平均薪資=",total/mycount)   
+
+avg({
+"count":3,
+"employees":[
+{
+"name":"John",
+"salary":30000
+},
+{
+"name":"Bob",
+"salary":60000
+},
+{
+"name":"Jenny",
+"salary":50000
+}
+]
+}) # 呼叫 avg 函式
